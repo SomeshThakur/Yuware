8c6dfcee90e5c8b89c64be473696c8577fed1052
Somesh Thakur <someshthakur33@gmail.com>
2015-10-30 22:28:08 +0530

    Addded Device selection
    And title.png for poster
    Signed off Somesh Thakur <someshthakur33@gmail.com>

--- src/yuware/Yuware.java
+++ src/yuware/Yuware.java
@@ -40,7 +40,13 @@ public class Yuware extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        Devices = new javax.swing.ButtonGroup();
         jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        DSelected = new javax.swing.JTextField();
+        Byureka = new javax.swing.JToggleButton();
+        Byuphoria = new javax.swing.JToggleButton();
+        Byunique = new javax.swing.JToggleButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem2 = new javax.swing.JMenuItem();
@@ -53,6 +59,33 @@ public class Yuware extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/yuware/title.png"))); // NOI18N
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
+        jLabel2.setText("Select your device. Current working device:");
+
+        Devices.add(Byureka);
+        Byureka.setText("Yureka / Yureka+");
+        Byureka.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ByurekaActionPerformed(evt);
+            }
+        });
+
+        Devices.add(Byuphoria);
+        Byuphoria.setText("Yuphoria");
+        Byuphoria.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ByuphoriaActionPerformed(evt);
+            }
+        });
+
+        Devices.add(Byunique);
+        Byunique.setText("Yunique");
+        Byunique.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ByuniqueActionPerformed(evt);
+            }
+        });
 
         jMenu1.setText("Thread");
 
@@ -102,8 +135,23 @@ public class Yuware extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addComponent(jLabel1)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(DSelected, javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                .addComponent(Byureka)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(Byuphoria)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(Byunique)))))
                 .addContainerGap(468, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -111,7 +159,16 @@ public class Yuware extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(29, 29, 29)
                 .addComponent(jLabel1)
-                .addContainerGap(333, Short.MAX_VALUE))
+                .addGap(71, 71, 71)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(DSelected, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Byureka)
+                    .addComponent(Byuphoria)
+                    .addComponent(Byunique))
+                .addContainerGap(193, Short.MAX_VALUE))
         );
 
         pack();
@@ -142,6 +199,21 @@ public class Yuware extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
+    private void ByurekaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ByurekaActionPerformed
+        DSelected.setText("Yureka or Yureka plus selected!");
+        Byureka.setSelected(true);  
+    }//GEN-LAST:event_ByurekaActionPerformed
+
+    private void ByuphoriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ByuphoriaActionPerformed
+DSelected.setText("Yuphoria selected!");
+        Byuphoria.setSelected(true);
+    }//GEN-LAST:event_ByuphoriaActionPerformed
+
+    private void ByuniqueActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ByuniqueActionPerformed
+        DSelected.setText("Yunique selected!");
+        Byunique.setSelected(true);       
+    }//GEN-LAST:event_ByuniqueActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -158,15 +230,11 @@ public class Yuware extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Yuware.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Yuware.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Yuware.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(Yuware.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        
         //</editor-fold>
 
         /* Create and display the form */
@@ -182,7 +250,13 @@ public class Yuware extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton Byunique;
+    private javax.swing.JToggleButton Byuphoria;
+    private javax.swing.JToggleButton Byureka;
+    private javax.swing.JTextField DSelected;
+    private javax.swing.ButtonGroup Devices;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
